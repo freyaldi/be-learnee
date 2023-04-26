f3907b6710b61a5a4bc8491d14b455165c66ba45
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-04-26 15:22:55 +0700

    feat: added auth_middleware - validateToken

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,54 @@
+package middleware
+
+import (
+	"fmt"
+	"strings"
+
+	"git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/usecase"
+	"git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/util"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const schema = "Bearer"
+
+func validateToken(encodedToken string) (*util.IdTokenClaims, error) {
+	claims := &util.IdTokenClaims{}
+	token, err := jwt.ParseWithClaims(encodedToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(util.SECRET_KEY), nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return nil, err
+		}
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
+
+func AuthorizeJWT(u usecase.UserUsecase) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		s := strings.Split(authHeader, fmt.Sprintf("%v ", schema))
+		
+		authError := util.UnauthorizedError()
+		
+		if len(s) < 2 {
+			ctx.AbortWithStatusJSON(authError.StatusCode, authError)
+			return
+		}
+
+		decodedToken := s[1]
+		claims, err := validateToken(decodedToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(authError.StatusCode, authError)
+			return
+		}
+
+		ctx.Set("auth", claims)
+		ctx.Next()
+	}
+}
