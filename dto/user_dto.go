6c4ff8138c88814ef59afbc5391a8b471e3de2db
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-04-26 15:24:49 +0700

    feat: added dto for user login request

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -8,3 +8,8 @@ type UserRegisterRequest struct {
 	PhoneNumber string `json:"phone_number" validate:"required,numeric,min=10,max=13"`
 	RefReferral string `json:"ref_referrral" validate:"omitempty"`
 }
+
+type UserLoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
