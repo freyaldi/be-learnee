606691d349e2ff1f4ae22c05758d0685c0b12d46
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-04-26 09:21:38 +0700

    feat: added user struct and level enum for user level

---
+++ entity/user.go
@@ -0,0 +1,23 @@
+package entity
+
+type User struct {
+	Id          int    `gorm:"primaryKey"`
+	Email       string `gorm:"unique"`
+	Password    string
+	IsAdmin     bool
+	Fullname    string
+	Address     string
+	PhoneNumber string
+	Level       level
+	Referral    string
+	RefReferral *string
+}
+
+type level string
+
+const (
+	Newbie level = "newbie"
+	Junior level = "junior"
+	Senior level = "senior"
+	Master level = "master"
+)
