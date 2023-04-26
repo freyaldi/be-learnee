b7cb0930bca2cb4e732cc177242d5feae44ae7a6
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-04-26 15:24:27 +0700

    feat: added new sentinel error - userNotFound, incorrectCredentials

--- error/error.go
+++ error/error.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	ErrUserAlreadyExists    = errors.New("user already exists")
+	ErrUserNotFound         = errors.New("user not found")
+	ErrIncorrectCredentials = errors.New("incorrect email or password")
 )
