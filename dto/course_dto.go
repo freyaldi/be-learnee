1c1ee9532a8ac7bc448cdd7ada2e7ed1ee9efa61
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-04-27 11:26:52 +0700

    feat: added course detail response dto

---
+++ dto/course_dto.go
@@ -0,0 +1,11 @@
+package dto
+
+type CourseDetailResponse struct {
+	Title              string `json:"title"`
+	Slug               string `json:"slug:"`
+	SummaryDescription string `json:"summary_description"`
+	Content            string `json:"content"`
+	ImgThumbnail       string `json:"img_thumbnail"`
+	ImgUrl             string `json:"img_url"`
+	AuthorName         string `json:"author_name"`
+}
