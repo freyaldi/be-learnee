8a07a1ec5fc8041f2e9a68c4470ae5dd7097c4df
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-05 10:58:09 +0700

    feat: add sentinel error course already carted

--- error/error.go
+++ error/error.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrUserAlreadyExists        = errors.New("user already exists")
 	ErrIncorrectCredentials     = errors.New("incorrect email or password")
+	ErrCourseAlreadyCarted      = errors.New("course already carted")
 	ErrCourseAlreadyFavorited   = errors.New("course already favorited")
 	ErrCourseAlreadyUnFavorited = errors.New("course already unfavorited")
 )
