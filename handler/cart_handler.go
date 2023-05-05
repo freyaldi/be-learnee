18770f56686d8cd665cc22c2aa2f01a6740518b4
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-05 14:21:48 +0700

    feat: add RemoveFromCart handler

--- handler/cart_handler.go
+++ handler/cart_handler.go
@@ -8,6 +8,7 @@ import (
 	er "git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/error"
 	"git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/util"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func (h *Handler) AddToCart(ctx *gin.Context) {
@@ -36,4 +37,32 @@ func (h *Handler) AddToCart(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, util.SuccessResponse("Course is added into cart successfully", http.StatusCreated, nil))
+}
+
+func (h *Handler) RemoveFromCart(ctx *gin.Context) {
+	userId := ctx.GetInt("user_id")
+
+	request := &dto.RemoveFromCartRequest{}
+	if err := ctx.ShouldBindJSON(request); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, util.ErrorResponse(err.Error(), http.StatusBadRequest))
+		return
+	}
+
+	err := util.Validate(request)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, util.ErrorResponse(err.Error(), http.StatusBadRequest))
+		return
+	}
+
+	err = h.cartUsecase.RemoveFromCart(userId, request.CourseId)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, util.ErrorResponse("course is not found in cart", http.StatusBadRequest))
+			return
+		}
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, util.ErrorResponse(err.Error(), http.StatusInternalServerError))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, util.SuccessResponse("Course is removed from cart successfully", http.StatusOK, nil))
 }
