f19affe357e258ded0166749840ac0a2f5707167
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-05 14:20:16 +0700

    feat: add RemoveFromCartRequest dto

--- dto/cart_dto.go
+++ dto/cart_dto.go
@@ -3,3 +3,7 @@ package dto
 type AddToCartRequest struct {
 	CourseId int `json:"course_id" validate:"required"`
 }
+
+type RemoveFromCartRequest struct {
+	CourseId int `json:"course_id" validate:"required"`
+}
