dc03e12b68a1292635db909cdeb4381e6e2bebb0
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-09 13:22:51 +0700

    feat: add UpdateLevel function and modify UserLevelBenefit function to prevent using magic number

--- util/user_level.go
+++ util/user_level.go
@@ -1,18 +1,38 @@
 package util
 
-import "git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/entity"
+import (
+	"git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/constant"
+	"git.garena.com/sea-labs-id/batch-06/ferza-reyaldi/stage01-project-backend/entity"
+)
 
 func UserLevelBenefit(user *entity.User) float32 {
 	var benefit float32
 	if user.Level == entity.Newbie {
-		benefit = 0
+		benefit = constant.LevelBenefitNewbie
 	} else if user.Level == entity.Junior {
-		benefit = 0.05
+		benefit = constant.LevelBenefitJunior
 	} else if user.Level == entity.Senior {
-		benefit = 0.1
+		benefit = constant.LevelBenefitSenior
 	} else if user.Level == entity.Master {
-		benefit = 0.2
+		benefit = constant.LevelBenefitMaster
 	}
 	return benefit
+}
+func UpdateLevel(level string, totalTransaction int) string {
+	if level == string(entity.Newbie){
+		if totalTransaction >= constant.MinTotalTransactionJunior {
+			level = string(entity.Junior)
+		}
+	} 
+	if level == string(entity.Junior){
+		if totalTransaction >= constant.MinTotalTransactionSenior {
+			level = string(entity.Senior)
+		}
+	} else if level == string(entity.Senior){
+		if totalTransaction >= constant.MinTotalTransactionMaster {
+			level = string(entity.Master)
+		}
+	}
+	return level
 }
 
