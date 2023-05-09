200242b6f2e121807cfcdddae0f54e2735bb86fb
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-09 13:21:42 +0700

    feat: add constant.go, store all of constant value

---
+++ constant/constant.go
@@ -0,0 +1,12 @@
+package constant
+
+const (
+	LevelBenefitNewbie = 0.0
+	LevelBenefitJunior = 0.05
+	LevelBenefitSenior = 0.1
+	LevelBenefitMaster = 0.2
+
+	MinTotalTransactionJunior = 10
+	MinTotalTransactionSenior = 20
+	MinTotalTransactionMaster = 50
+)
